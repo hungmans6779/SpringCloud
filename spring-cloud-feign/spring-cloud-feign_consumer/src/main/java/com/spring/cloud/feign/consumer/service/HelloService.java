2934f49f541da9c2e6b0eacd509633baed3c0cc1
chiahung.hung <chiahung.hung@fubon.com>
2021-07-14 10:32:30 +0800

    Spring Cloud

---
+++ spring-cloud-feign/spring-cloud-feign_consumer/src/main/java/com/spring/cloud/feign/consumer/service/HelloService.java
@@ -0,0 +1,66 @@
+/**
+ * KK Owner CONFIDENTIAL/
+ * 
+ * @Package: com.spring.cloud.hystrix.ribbon.service
+ * @FileName: HelloService.java
+ * @author: chiahung.hung
+ * @date: 2020年12月29日, 下午3:23:02
+ * 
+ *        <pre>
+ *  Copyright 2013-2014 The KK Owner Co., Ltd. all rights reserved.
+ *
+ *  NOTICE:  All information contained herein is, and remains
+ *  the property of KK Owner Co., Ltd. and its suppliers,
+ *  if any.  The intellectual and technical concepts contained
+ *  herein are proprietary to KK Owner Co., Ltd. and its suppliers
+ *  and may be covered by TAIWAN and Foreign Patents, patents in 
+ *  process, and are protected by trade secret or copyright law.
+ *  Dissemination of this information or reproduction of this material
+ *  is strictly forbidden unless prior written permission is obtained
+ *  from KK Owner Co., Ltd.
+ *  </pre>
+ */
+package com.spring.cloud.feign.consumer.service;
+
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+/**
+ * <pre> HelloService, TODO: add Class Javadoc here. </pre>
+ *
+ * @author chiahung.hung
+ */
+@Service
+public class HelloService {
+
+  /** The logger. */
+  private final Logger logger = Logger.getLogger(getClass());
+
+  @Autowired
+  RestTemplate restTemplate;
+  
+  
+  
+  public String helloService() {
+    
+    long start = System.currentTimeMillis();
+    
+    ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://hello-service/hello", String.class);
+    String body = responseEntity.getBody();
+
+    long end = System.currentTimeMillis();
+    
+    
+    logger.info("Spend time : " + (end - start));
+    
+    return body.toString();
+  } // end helloService
+
+ 
+  
+
+}
---
+++ 【批次】/spring-cloud-feign/02_2 spring-cloud-feign_eureka_client (hello) (port 8082).bat
@@ -0,0 +1,1 @@
+java -jar jar/spring-cloud-feign_eureka_client-0.0.1-SNAPSHOT.jar --server.port=8082 --eureka.instance.hostname=provider2
